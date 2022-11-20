c0cb71d58ff0f367255f966b7c23f0a86944c711
Harry Steele <harrysteele@Harrys-MacBook-Air.local>
2022-11-19 23:22:03 -0500

    finished exercise

--- src/EightBall.js
+++ src/EightBall.js
@@ -1,19 +1,32 @@
-import React, {useState} from "react";
+import React, { useState } from 'react';
 import './Eightball.css'
 
 const EightBall = ({data}) => {
+    const [saying, setSaying] = useState("Think of a Question")
+    const [color, setColor] = useState("black")
 
-    const randomFunc = (arr) => {
-        return arr[Math.floor(Math.random() * arr.length)]
+    const random = (arr) => {
+        const random =  arr[Math.floor(Math.random() * arr.length)]
+        return [random.color, random.msg]
     }
 
-    const {saying, setSaying} = useState("Think of a Question")
-    const {color, setColor} = useState("green")
+    // const randomSaying = (arr) => {
+    //     const random =  arr[Math.floor(Math.random() * arr.length)]
+    //     return random.msg
+    // }
+
+    const UpdateBall = () => {
+        const ballData = random(data)
+
+        setColor(ballData[0])
+        setSaying(ballData[1])
+    }
+
     return (
         <div>
             <div className="eightball" style={{backgroundColor: color}}>{saying}</div>
 
-            <button onClick={() => randomFunc(data)}>Choose a new saying</button>
+            <button onClick={() => UpdateBall()}>Choose a new saying</button>
         </div>
     )
 }
